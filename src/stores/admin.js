f6916e6d4a149a9a45d1b707137f3cf4ffb8eb21
Zhang Fei <1848885643@qq.com>
2026-06-18 18:47:21 +0800

    docs: 测试提交

--- src/stores/admin.js
+++ src/stores/admin.js
@@ -1,13 +1,13 @@
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
 
-
+// wq?
 export const UseAdminStore = defineStore('admin', () => {
   const isCollapse = ref(false)
 
   const toggleCollapse = () => {
     isCollapse.value = !isCollapse.value
-  } 
+  }
 
   return {
     isCollapse,
